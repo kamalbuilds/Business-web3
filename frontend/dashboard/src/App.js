0dbb1276a128fddf90ad2393779afa51cf354981
Kamal Nayan <geniusamansingh@gmail.com>
2022-05-28 11:58:38 +0000

    feature section for business people to add on

--- frontend/dashboard/src/App.js
+++ frontend/dashboard/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar.js';
+import Features from './components/Features.js';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Features />
     </div>
   );
 }
