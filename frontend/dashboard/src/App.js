142c4206686b042d816e7c2fd89033b8606cb49f
Abhijeet Kumar <63132585+Abhijeet1520@users.noreply.github.com>
2022-05-29 03:39:11 +0530

    Add routing

--- frontend/dashboard/src/App.js
+++ frontend/dashboard/src/App.js
@@ -1,17 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar.js';
 import Features from './components/Features.js';
 import Register from './components/Register.js';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Navbar />
-      <Features />
-      <Register />
-      
-    </div>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Features/>} />
+          <Route path="/discounts" element={<Navbar/>} />
+          <Route path="/register" element={<Register/>} />
+        </Routes>
+      </Router>
   );
 }
 
