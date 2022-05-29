58a598be098335e432637505e50deb9a36c0985f
Kamal Nayan <geniusamansingh@gmail.com>
2022-05-29 03:59:17 +0000

    banner and product listing added

--- frontend/dashboard/src/App.js
+++ frontend/dashboard/src/App.js
@@ -2,15 +2,19 @@ import './App.css';
 import Navbar from './components/Navbar.js';
 import Features from './components/Features.js';
 import Register from './components/Register.js';
+import Products from './components/Productlisting.js';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Banner from './components/Banner.js'
+import Main from './components/Main1.js';
 
 const App = () => {
   return (
       <Router>
         <Routes>
-          <Route exact path="/" element={<Features/>} />
-          <Route path="/discounts" element={<Navbar/>} />
-          <Route path="/register" element={<Register/>} />
+          <Route exact path="/" element={<div>  <Main /> </div>} />
+          <Route exact path="/customer" element={<Features/>} />
+          <Route path="/discounts" element={<div> <Navbar/> ,<Banner />, <Products /></div>} />
+          <Route path="/register" element={<div> <Navbar />,<Register/> </div>} />
         </Routes>
       </Router>
   );
